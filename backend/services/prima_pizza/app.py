45f99978a895653496bed9770fb5ae3ff56676dc
Daniel Vazquez <da.vazquez728@gmail.com>
2025-03-04 02:57:13 -0700

    deployment attempt backend only 2.7

--- backend/services/prima_pizza/app.py
+++ backend/services/prima_pizza/app.py
@@ -42,6 +42,12 @@ def create_app():
     return app
 
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=settings.LAYER_PORT, debug=secrets.DEBUG)
+    app.run(
+        host="0.0.0.0",
+        port=settings.LAYER_PORT,
+        debug=secrets.DEBUG,
+        ssl_context=("cert.pem", "key.pem"),
+    )
