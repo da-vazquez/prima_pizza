f0aa3622d98f59518800e9bee5486885e62a91e5
Daniel V. <71048973+da-vazquez@users.noreply.github.com>
2025-03-05 02:25:11 -0700

    Frontend updates prod (#13)
    * updated frontend urls and envs to interact with prod instances
    
    * deploy to vercel

--- frontend/prima_pizza/src/api/mongodb.ts
+++ frontend/prima_pizza/src/api/mongodb.ts
@@ -1,23 +1,26 @@
 import { MongoClient } from "mongodb";
 
-const uri= process.env.NEXT_PUBLIC_MONGODB_URI
-console.log("connection: ", uri)
+const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
 
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+if (!uri) {
+  throw new Error(
+    "Please define the NEXT_PUBLIC_MONGODB_URI environment variable inside .env.local"
+  );
+}
 
-console.log("client: ", client)
+const client = new MongoClient(uri);
 
-let clientPromise;
+let clientPromise: Promise<MongoClient>;
 
-if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
+  // In production mode, it's best to not use a global variable.
   clientPromise = client.connect();
 }
 
