b1a5665e6425276744e0dbce6287a31a841a3627
Daniel Vazquez <da.vazquez728@gmail.com>
2025-03-04 23:31:53 -0700

    gunicorn code updates

--- backend/services/prima_pizza/db.py
+++ backend/services/prima_pizza/db.py
@@ -12,20 +12,10 @@ from instance import secrets
 
 load_dotenv()
 
-database_url = (
-    os.getenv("TEST_DATABASE_URL")
-    if os.getenv("ENV") == "TEST"
-    else os.getenv("DATABASE_URL")
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-client = MongoClient(database_url)
-db = client["prima_pizza"]
+client = MongoClient(DATABASE_URL, tls=True, tlsAllowInvalidCertificates=True)
 
-if os.getenv("ENV") == "TEST":
-    toppings_collection = db.test_toppings
-    pizzas_collection = db.test_pizzas
-    users_collection = db.test_users
-else:
-    toppings_collection = db.toppings
-    pizzas_collection = db.pizzas
-    users_collection = db.users
+db = client.get_database()
+
+users_collection = db.get_collection("users")
