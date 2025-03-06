9c042f4e7ffe53e154b50d5410c69be7d2137f93
Daniel V. <71048973+da-vazquez@users.noreply.github.com>
2025-03-05 23:54:15 -0700

    Prod (#21)
    * updated with logging
    
    * updated with logging
    
    * updated with logging
    
    * updated with logging
    
    * updated with logging
    
    * updated with logging
    
    * updated with api

--- backend/services/prima_pizza/app.py
+++ backend/services/prima_pizza/app.py
@@ -4,11 +4,11 @@ Default Imports
 import logging
 from datetime import timedelta
 from flask import Flask, request, redirect, jsonify
-from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 import warnings
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 """
 Logging
@@ -31,59 +31,18 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
 
-    default_origins = [
-        "https://prima-pizza.vercel.app",
-        "https://prima-pizza-backend-west.azurewebsites.net",
-        "http://localhost:3000",
-        "https://localhost:3000",
-    ]
-
     app.config.update(
         JWT_SECRET_KEY=secrets.JWT_SECRET_KEY,
         JWT_ACCESS_TOKEN_EXPIRES=timedelta(hours=3),
-        CORS_ORIGINS=default_origins,
     )
 
     jwt = JWTManager(app)
 
-    CORS(
-        app,
-        resources={
-            r"/*": {
-                "origins": default_origins,
-                "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-                "allow_headers": [
-                    "Content-Type",
-                    "Authorization",
-                    "Accept",
-                    "Origin",
-                    "X-Requested-With",
-                ],
-                "supports_credentials": True,
-            }
-        },
-        supports_credentials=True,
-    )
+    CORS(app, supports_credentials=True, origins="*")
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(toppings_bp)
     app.register_blueprint(pizzas_bp)
-
-    @app.after_request
-    def after_request(response):
-        origin = request.headers.get("Origin")
-        if origin in app.config["CORS_ORIGINS"]:
-            response.headers["Access-Control-Allow-Origin"] = origin
-            response.headers["Access-Control-Allow-Credentials"] = "true"
-            response.headers[
-                "Access-Control-Allow-Methods"
-            ] = "GET, POST, PUT, DELETE, OPTIONS"
-            response.headers[
-                "Access-Control-Allow-Headers"
-            ] = "Content-Type, Authorization, Accept, Origin, X-Requested-With"
-        if request.method == "OPTIONS":
-            response.status_code = 200
-        return response
 
     @app.errorhandler(Exception)
     def handle_error(error):
