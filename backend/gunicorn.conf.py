053e45030d1f966121b24b7c97bb068150510a1f
Daniel Vazquez <da.vazquez728@gmail.com>
2025-03-10 11:23:41 -0700

    cors updates and logging

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -2,8 +2,9 @@ import os
 import multiprocessing
 
 # Gunicorn config
-bind = "0.0.0.0:" + str(os.getenv("PORT", "8000"))
+bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
-timeout = 120
+timeout = 600
 keepalive = 5
+preload_app = True
 worker_class = "sync"
